7058133a99807b09446eb0ec084ec90c31060e6c
blakeh21 <95169432+blakeh21@users.noreply.github.com>
2023-09-06 02:18:40 +1000

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,7 @@ sidebar = st.sidebar
 col1, col2, col3 = st.columns(3)
 
 col1.header("Unread Emails/Follow Ups")
-col1.write("""
-         # Here are unread emails
-         This would be email content""")
+"# Here are unread emails This would be email content"
 respond = col1.button("Respond")
 if respond:
     col1.header("Unread Emails/Follow Ups 2")
@@ -16,12 +14,8 @@ if respond:
          # Here are unread emails
          This would be email content""")
 
+
 col2.header("Automation")
-col1.write("""
-         # Here are unread emails
-         This would be email content""")
+
 
 col3.header("Unread Emails")
-col1.write("""
-         # Here are unread emails
-         This would be email content""")
