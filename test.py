b277e302dab801413d05a5c4c56c4690808d6d84
blakeh21 <95169432+blakeh21@users.noreply.github.com>
2023-09-06 02:14:40 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,22 @@
 import streamlit as st
 
 # Initialize Streamlit and Gmail configurations
-st.write("# Email Tracker")
+st.set_page_title("Email Tracker")
 sidebar = st.sidebar
+col1, col2, col3 = st.columns(3)
 
-st.write("""
+col1.header("Unread Emails/Follow Ups")
+col1.write("""
+         # Here are unread emails
+         This would be email content""")
+want_to_contribute = col1.button("Respond")
+
+col2.header("Automation")
+col1.write("""
+         # Here are unread emails
+         This would be email content""")
+
+col3.header("Unread Emails")
+col1.write("""
          # Here are unread emails
          This would be email content""")
